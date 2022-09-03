d0948694195c3dc7a00c1391575328ed33991a5c
Jason Sturges <jason@jasonsturges.com>
2022-09-03 16:53:06 -0500

    SvelteKit implementation

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<main>
+	<slot />
+</main>
+
+<style>
+	:global(body) {
+		margin: 0;
+		padding: 0;
+		overflow: hidden;
+	}
+
+  :global(canvas) {
+    width: 100vw;
+    height: 100vh;
+    display: block;
+  }
+</style>
